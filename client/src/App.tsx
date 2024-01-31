37184d03a8673eac35a8cf3750f55bd446328cbd
Richard Angelo Jaro <48646118+CardoSanti@users.noreply.github.com>
2024-02-01 01:27:27 +0800

    Initialized client side for balt

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <div className='p-20'>
+        <Button>Hello ShadCN and Vite!</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
